9a2a0c8294952780c621720ad14b03fde011c0ee
Emdien <gonnm8@gmail.com>
2021-06-07 20:50:11 +0200

    Implementado validación parcial de la página de opciones
    - Falta por comprobar si la contraseña actual coincide con la indicada. (Ajax?)

--- Bualatok/public/javascripts/validation.js
+++ Bualatok/public/javascripts/validation.js
@@ -10,7 +10,7 @@ function check() {
     }
 }
 
-function passwordValidation() {
+function validation() {
     var password = document.getElementById('inputPassword').value;
     var rpassword = document.getElementById('inputRPassword').value;
 
@@ -29,13 +29,40 @@ function passwordValidation() {
     return true;
 }
 
+function creditValidation() {
+    var credito = document.getElementById('inputCredits');
+
+    if (credito.value == ''){
+        credito.value = 0;
+    }
+}
+
+
 
 window.onload = function() {
     var inputPassword = document.getElementById('inputPassword');
     var inputRPassword = document.getElementById('inputRPassword');
+    var registerForm = document.getElementById('registerform');
+    var passwordForm = document.getElementById('form-password');
+    var creditForm = document.getElementById('form-credito');
     
-    document.getElementById('registerform').onsubmit = passwordValidation;
+    if (registerForm != null || registerForm != undefined){
+        
+        registerForm.onsubmit = validation;     
+        
+    }
 
+    if (passwordForm != null || passwordForm != undefined){
+        
+        passwordForm.onsubmit = validation;     
+        
+    }
+    
+    if (creditForm != null || creditForm != undefined){
+        
+        creditForm.onsubmit = creditValidation;     
+        
+    }
     inputPassword.addEventListener('keyup', check);
     inputRPassword.addEventListener('keyup', check);
 
